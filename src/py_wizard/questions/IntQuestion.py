228df07e9ec833baee0bfb1717091dc4d5606599
Nathan Shearer <nshearer@ewu.edu>
2014-10-03 11:32:41 -0700

    Updated IntQuestion to take min and max values

--- src/py_wizard/questions/IntQuestion.py
+++ src/py_wizard/questions/IntQuestion.py
@@ -7,19 +7,30 @@ from SimpleQuestion import SimpleQuestion
 
 class IntQuestion(SimpleQuestion):
     '''A question where the answer is an integer'''
-    
-    def __init__(self, name, question, default=None):
+
+    def __init__(self, name, question, default=None, min_value=None, max_value=None):
+        self.__min_value = min_value
+        self.__max_value = max_value
         super(IntQuestion, self).__init__(name, question, default)
-        
-    
+
+
+
     def get_simple_question_child_error(self, answer):
-        
+
         if answer is not None:
             try:
                 int(answer)
             except ValueError:
                 return "Answer is not an integer"
-        
+
+            if self.__min_value is not None:
+                if int(answer) < self.__min_value:
+                    return "Answer must be greater than %d" % (self.__min_value)
+
+            if self.__max_value is not None:
+                if int(answer) > self.__max_value:
+                    return "Answer must be less than %d" % (self.__max_value)
+
         return None
-    
-    
+
+
